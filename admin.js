a5d2bf23eb969ad563f525f67cc3140b35b31cbb
Ifran Ruhin <ifrunruhin@gmail.com>
2025-08-06 19:30:42 +0600

    Set the backend api

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://blog-backend-qlgd.onrender.com/api';
 let authToken = localStorage.getItem('adminToken');
 let isEditing = false;
 
